ee8366ae110d99c6b48fe323166a0419adb39d31
Hind8779! <hanosalih2020@gmail.com>
2025-05-21 11:17:24 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("this is a new code");
+        System.out.println("hello");
+        
+
 
         }
     }
